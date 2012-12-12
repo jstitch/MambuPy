4254bb1b45f01610897efe04f04ab014a04b508d
Javier Novoa <jstitch@gmail.com>
2012-12-11 19:14:58 -0600

    mambu transactions may be in an iterable list

--- mambutransaction.py
+++ mambutransaction.py
@@ -1,4 +1,4 @@
-from mambustruct import MambuStruct
+from mambustruct import MambuStruct, MambuStructIterator
 from podemos import PodemosError, DEBUG, ERROR_CODES
 from datetime import datetime
 
@@ -34,6 +34,23 @@ from datetime import datetime
 # }
 
 
+# Objeto con una lista de Transacciones Mambu
+class MambuTransactions(MambuStruct):
+    def __iter__(self):
+        return MambuStructIterator(self.attrs)
+
+    def __getitem__(self, key):
+        return self.attrs[key]
+
+    def __len__(self):
+        return len(self.attrs)
+
+    def convertDict2Attrs(self):
+        for t in self.attrs:
+            trans = MambuTransaction(urlfunc=None, entid=None)
+            trans.init(t)
+            t = trans
+
 # Objeto con una Transaccion de Mambu
 class MambuTransaction(MambuStruct):
     # De un diccionario de valores como cadenas, convierte los pertinentes a numeros/fechas
@@ -41,14 +58,35 @@ class MambuTransaction(MambuStruct):
         try:
             self.attrs['transactionId'] = self.attrs['transactionId']
             self.attrs['type'] = self.attrs['type']
-            self.attrs['amount'] = float(self.attrs['amount'])
-            self.attrs['comment'] = self.attrs['comment']
-            self.attrs['balance'] = float(self.attrs['balance'])
+            try:
+                self.attrs['amount'] = float(self.attrs['amount'])
+            except KeyError as kerr:
+                pass
+            try:
+                self.attrs['comment'] = self.attrs['comment']
+            except KeyError as kerr:
+                pass
+            try:
+                self.attrs['balance'] = float(self.attrs['balance'])
+            except KeyError as kerr:
+                pass
 
-            self.attrs['principalPaid'] = float(self.attrs['principalPaid'])
-            self.attrs['interestPaid'] = float(self.attrs['interestPaid'])
-            self.attrs['feesPaid'] = float(self.attrs['feesPaid'])
-            self.attrs['penaltyPaid'] = float(self.attrs['penaltyPaid'])
+            try:
+                self.attrs['principalPaid'] = float(self.attrs['principalPaid'])
+            except KeyError as kerr:
+                pass
+            try:
+                self.attrs['interestPaid'] = float(self.attrs['interestPaid'])
+            except KeyError as kerr:
+                pass
+            try:
+                self.attrs['feesPaid'] = float(self.attrs['feesPaid'])
+            except KeyError as kerr:
+                pass
+            try:
+                self.attrs['penaltyPaid'] = float(self.attrs['penaltyPaid'])
+            except KeyError as kerr:
+                pass
 
             try:
                 self.attrs['transactionDate'] = datetime.strptime(self.attrs['transactionDate'], "%Y-%m-%dT%H:%M:%S+0000")
