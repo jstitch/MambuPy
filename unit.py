6869ef68541426e3257f23e4914a09850de860c3
Javier Novoa <jstitch@gmail.com>
2014-04-08 00:24:18 -0500

    Unit test MambuStruct init and connect methods

--- unit.py
+++ unit.py
@@ -1,12 +1,70 @@
 import mock
 import unittest
 
-class MambuStructTests(unittest.TestCase):
-    def test_init(self):
-        from mambustruct import MambuStruct
+import mambustruct
 
-        s = MambuStruct(entid='1234', urlfunc=lambda x:x, connect=False)
+class MambuStructTests(unittest.TestCase):
+    def setUp(self):
+        s = mambustruct.MambuStruct(entid='1234', urlfunc=lambda x:x, connect=False)
+        s.rc.reset()
+        self.func = lambda x:'http://example.com'+x
+
+    def test_init(self):
+        # Normal init
+        s = mambustruct.MambuStruct(entid='1234', urlfunc=self.func, connect=False)
         self.assertEqual(s.entid, '1234')
+        self.assertEqual(s.rc.cnt, 0)
+        
+        # Test that urlfunc=None sets no attributes
+        s = mambustruct.MambuStruct(entid='1234', urlfunc=None)
+        self.assertDictEqual(s.__dict__, {})
+
+    @mock.patch('mambustruct.json')
+    @mock.patch('mambustruct.urlopen')
+    def test_connect(self, urlopen, json):
+        json.load.side_effect = [{},
+                                 {'returnCode'  :'TestErrorCode',
+                                  'returnStatus':'Test Error Status',},
+                                 {}]
+        
+        s = mambustruct.MambuStruct(entid='1234', urlfunc=self.func, connect=False)
+        s.connect()
+
+        # urlopen is called to open the URL given by func
+        urlopen.assert_called_with(self.func('1234'))
+        self.assertEqual(s.rc.cnt, 1)
+
+        # simulate Mambu returns an error
+        with self.assertRaisesRegexp(mambustruct.MambuError, "^Test Error Status$") as ex:
+            s.connect()
+        self.assertEqual(s.rc.cnt, 2)
+
+        s = mambustruct.MambuStruct(entid='1234', urlfunc=self.func)
+        self.assertEqual(s.rc.cnt, 3)
+
+        
+    @mock.patch('mambustruct.json')
+    @mock.patch('mambustruct.urlopen')
+    def test_init(self, urlopen, json):
+        json.load.side_effect = [{'attr1': 1,
+                                  'attr2': 3.141592,
+                                  'attr3': "string",
+                                 }]
+
+        s = mambustruct.MambuStruct(entid='1234', urlfunc=self.func)
+        self.assertEqual(s.attrs['attr1'], 1)
+        self.assertEqual(s.attrs['attr2'], 3.141592)
+        self.assertEqual(s.attrs['attr3'], "string")
+
+        # Dict-like behaviour
+        self.assertEqual(s['attr1'],1)
+        s['attr3'] = "hello world!"
+        self.assertEqual(s.attrs['attr3'], "hello world!")
+        self.assertTrue(s.has_key('attr2'))
+        self.assertListEqual(sorted(s.items()), [('attr1',1),
+                                                 ('attr2',3.141592),
+                                                 ('attr3',"hello world!")])
+
 
 if __name__ == '__main__':
     test_classes_to_run = [
