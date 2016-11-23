5506ea05d54cf18c11bd0dca780f3b618bbee213
Mauricio Garces <m.garces@podemos.mx>
2016-11-23 16:45:55 -0600

    Schema repayments was separated into a different file

--- schema_loans.py
+++ schema_loans.py
@@ -97,35 +97,6 @@ class LoanAccount(Base):
         return "<LoanAccount(id=%s, accountstate=%s)>" % (self.id, self.accountstate)
 
 
-class Repayment(Base):
-    """Repayment table.
-    """
-    __tablename__    = "repayment"
-    __table_args__   = {'schema'        : dbname,
-                        'keep_existing' : True
-                       }
-
-    # Columns
-    encodedkey       = Column(String, primary_key=True)
-    duedate          = Column(DateTime, index=True)
-    state            = Column(String, index=True)
-    principaldue     = Column(Numeric(50,10))
-    principalpaid    = Column(Numeric(50,10))
-    interestdue      = Column(Numeric(50,10))
-    interestpaid     = Column(Numeric(50,10))
-    feesdue          = Column(Numeric(50,10))
-    feespaid         = Column(Numeric(50,10))
-    penaltydue       = Column(Numeric(50,10))
-    penaltypaid      = Column(Numeric(50,10))
-
-    # Relationships
-    parentaccountkey = Column(String, ForeignKey(LoanAccount.encodedkey))
-    account          = relationship('LoanAccount', backref=backref('repayments',order_by='Repayment.duedate'))
-
-    def __repr__(self):
-        return "<Repayment(duedate=%s, state=%s,\naccount=%s)>" % (self.duedate.strftime('%Y%m%d'), self.state, self.account)
-
-
 class LoanTransaction(Base):
     """LoanTransaction table.
     """
