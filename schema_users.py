91404186daaf7c45cbb78e43d86f985fd7c6e6fa
Mauricio Garces <m.garces@podemos.mx>
2017-03-16 12:26:40 -0600

    schema_users relationships changes to back_populates

--- schema_users.py
+++ schema_users.py
@@ -5,9 +5,8 @@ TODO: this are just very basic schemas for users.
 
 import schema_orm as orm
 from schema_branches import Branch
-from schema_customfields import CustomFieldValue
 
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
 from sqlalchemy import Column, String, DateTime, Numeric, Integer
 
@@ -26,7 +25,8 @@ class Role(Base):
 
     # Columns
     encodedkey = Column(String, primary_key=True)
-    name      = Column(String)
+    name       = Column(String)
+    users      = relationship('User', back_populates='role')
 
     def __repr__(self):
         return "<Role(name={})>".format(self.name)
@@ -54,15 +54,23 @@ class User(Base):
 
     # Relationships
     assignedbranchkey = Column(String, ForeignKey(Branch.encodedkey))
-    branch            = relationship(Branch, backref=backref('users'))
+    branch            = relationship(Branch, back_populates='users')
 
     role_encodedkey_oid = Column(String, ForeignKey(Role.encodedkey))
-    role                = relationship(Role, backref=backref('users'))
+    role                = relationship(Role, back_populates='users')
 
-    custominformation   = relationship(CustomFieldValue,
-                                       backref=backref('user'),
-                                       foreign_keys=[CustomFieldValue.parentkey],
-                                       primaryjoin='CustomFieldValue.parentkey == User.encodedkey')
+    custominformation   = relationship('CustomFieldValue',
+                                       back_populates = 'user',
+                                       foreign_keys   = 'CustomFieldValue.parentkey',
+                                       primaryjoin    = 'CustomFieldValue.parentkey == User.encodedkey')
+    activities          = relationship('Activity',
+                                       primaryjoin    = 'User.encodedkey==Activity.userkey',
+                                       back_populates = 'user')
+    activities          = relationship('Activity',
+                                       primaryjoin    = 'User.encodedkey==Activity.assigneduserkey',
+                                       back_populates = 'assigneduser')
+    groups              = relationship('Group', back_populates = 'user')
+    loans               = relationship('LoanAccount', back_populates = 'user')
 
     @property
     def name(self):
