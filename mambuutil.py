46663be256fef3d9c5c7da77104d87b8475301c2
Javier Novoa C <jstitch@gmail.com>
2017-11-21 14:01:24 -0600

    Adding mambuutil getclientloansurl urlfunc.

--- mambuutil.py
+++ mambuutil.py
@@ -280,7 +280,7 @@ def getgroupurl(idgroup, *args, **kwargs):
     return url
 
 def getgrouploansurl(idgroup, *args, **kwargs):
-    """Request loan Repayments URL.
+    """Request Group loans URL.
 
     How to use it? By default MambuLoan uses getloansurl as the urlfunc.
     Override that behaviour by sending getgrouploansurl (this function)
@@ -414,6 +414,47 @@ def getclienturl(idclient, *args, **kwargs):
             pass
     clientidparam = "" if idclient == "" else "/"+idclient
     url = getmambuurl(*args,**kwargs) + "clients" + clientidparam + ( "" if len(getparams) == 0 else "?" + "&".join(getparams) )
+    return url
+
+def getclientloansurl(idclient, *args, **kwargs):
+    """Request Client loans URL.
+
+    How to use it? By default MambuLoan uses getloansurl as the urlfunc.
+    Override that behaviour by sending getclientloansurl (this function)
+    as the urlfunc to the constructor of MambuLoans (note the final 's')
+    and voila! you get the Loans just for a certain client.
+
+    If idclient is set, you'll get a response adequate for a
+    MambuLoans object.
+
+    If not set, you'll get a Jar Jar Binks object, or something quite
+    strange and useless as JarJar. A MambuError must likely since I
+    haven't needed it for anything but for loans of one and just
+    one client.
+
+    See mambuloan module and pydoc for further information.
+
+    Currently implemented filter parameters:
+    * accountState
+
+    See Mambu official developer documentation for further details, and
+    info on parameters that may be implemented here in the future.
+    """
+    getparams = []
+    if kwargs:
+        try:
+            if kwargs["fullDetails"] == True:
+                getparams.append("fullDetails=true")
+            else:
+                getparams.append("fullDetails=false")
+        except Exception as ex:
+            pass
+        try:
+            getparams.append("accountState=%s" % kwargs["accountState"])
+        except Exception as ex:
+            pass
+    clientidparam = "/" + idclient
+    url = getmambuurl(*args,**kwargs) + "clients" + clientidparam  + "/loans" + ( "" if len(getparams) == 0 else "?" + "&".join(getparams) )
     return url
 
 def getuserurl(iduser, *args, **kwargs):
