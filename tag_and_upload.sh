c27670971c83e5fd17eddb81cdc06881049aed8c
Javier Novoa <jstitch@gmail.com>
2022-02-22 20:36:38 -0600

    Tag and upload echo stages

--- tag_and_upload.sh
+++ tag_and_upload.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+echo "getting version"
 mambupy_version=`python -c "import MambuPy; print(MambuPy.__version__)"`
+
+echo "tagging"
 git tag v$mambupy_version
+
+echo "pushing tags"
 git push gitlab v$mambupy_version
 git push github v$mambupy_version
 git push bitbucket v$mambupy_version
 
+echo "building wheel, egg and sdist"
 python setup.py bdist_wheel bdist_egg sdist
+
+echo "uploading to pypi"
 twine upload dist/MambuPy-$mambupy_version*
