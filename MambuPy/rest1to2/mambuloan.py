4b34e7139f962740bdf8275225727b7a139f6b9a
Javier Novoa <jstitch@gmail.com>
2022-06-20 12:59:54 -0500

    feature: restito MambuLoan methods and urlfunc=getgrouploansurl().
    Methods:
    - setBranch
    - setCentre
    - setUser
    - setProduct
    - getDebt
    
    v1 MambuLoan urlfunc allows changing the endpoint so that retrieved
    loans are get from a certain group instead of all of Mambu.
    
    v2 supports this same functionality by using simple filters to the
    getAll endpoint

--- MambuPy/rest1to2/mambuloan.py
+++ MambuPy/rest1to2/mambuloan.py
@@ -1,6 +1,7 @@
 from mambupy.rest.mambuloan import MambuLoan as MambuLoan1, MambuLoans as MambuLoans1
 from mambupy.rest1to2.mambustruct import MambuStruct, process_filters
 from mambupy.rest.mambustruct import MambuStructIterator
+from mambupy.mambuutil import getgrouploansurl, MambuError
 
 
 loan_filters = ["accountState", "branchId", "centreId", "creditOfficerUsername"]
@@ -8,12 +9,65 @@ loan_filters = ["accountState", "branchId", "centreId", "creditOfficerUsername"]
 
 class MambuLoan(MambuStruct, MambuLoan1):
     def __init__(self, *args, **kwargs):
+        if "urlfunc" in kwargs and kwargs["urlfunc"] == getgrouploansurl:
+            kwargs.pop("urlfunc")
+            kwargs["accountHolderType"] = "GROUP"
+            kwargs["accountHolderId"] = kwargs.pop("entid")
         process_filters(loan_filters, kwargs)
         super().__init__(*args, **kwargs)
+
+    def getDebt(self):
+        self.principalBalance = self.balances["principalBalance"]
+        self.interestBalance = self.balances["interestBalance"]
+        self.feesBalance = self.balances["feesBalance"]
+        self.penaltyBalance = self.balances["penaltyBalance"]
+
+        return MambuLoan1.getDebt(self)
+
+    def setBranch(self, *args, **kwargs):
+        from mambupy.rest1to2 import mambubranch
+        self.branch = mambubranch.MambuBranch(
+            entid=self.wrapped2.assignedBranchKey, *args, **kwargs)
+        self.assignedBranchName = self.branch.name
+        self.assignedBranch = self.branch
+
+        return 1
+
+    def setCentre(self, *args, **kwargs):
+        from mambupy.rest1to2 import mambucentre
+        self.centre = mambucentre.MambuCentre(
+            entid=self.wrapped2.assignedCentreKey, *args, **kwargs)
+        self.assignedCentreName = self.centre.name
+        self.assignedCentre = self.centre
+
+        return 1
+
+    def setUser(self, *args, **kwargs):
+        from mambupy.rest1to2 import mambuuser
+        try:
+            self.user = mambuuser.MambuUser(
+                entid=self.wrapped2.assignedUserKey, *args, **kwargs)
+        except AttributeError:
+            err = MambuError("La cuenta %s no tiene asignado un usuario" % self.id)
+            err.noUser = True
+            raise err
+
+        return 1
+
+    def setProduct(self, *args, **kwargs):
+        from mambupy.rest1to2 import mambuproduct
+        self.product = mambuproduct.MambuProduct(
+            entid=self.wrapped2.productTypeKey, *args, **kwargs)
+
+        return 1
 
 
 class MambuLoans(MambuStruct, MambuLoans1):
     def __init__(self, *args, **kwargs):
+        if "urlfunc" in kwargs and kwargs["urlfunc"] == getgrouploansurl:
+            kwargs.pop("urlfunc")
+            kwargs["accountHolderType"] = "GROUP"
+            kwargs["accountHolderId"] = kwargs.pop("entid")
         if "mambuclassname" in kwargs:
             mambuclassname = kwargs.pop("mambuclassname")
         else:
